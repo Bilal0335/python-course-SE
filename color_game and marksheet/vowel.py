ec26a4be055c9b612e85c4aa944cbe174a9a43d9
bilal <bilalhussain42201@gmail.com>
2023-10-06 20:03:56 +0500

    color game and mark sheet

---
+++ color_game and marksheet/vowel.py
@@ -0,0 +1,61 @@
+# print("Python Program to Check Alphabet for Vowel or Consonant ")
+# alphabet = input("Enter a Character: ")
+# if alphabet in ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'):
+#                print(alphabet, "is Vowel")
+# else:
+#         print(alphabet, "is Consonant")
+
+
+# =============mark sheet===============
+# maths = int(input("Input your Maths Marks: "))
+# computer = int(input("Input your Computer Marks: "))
+# total = maths + computer
+# avg = float(total / 2)
+# per = float(total*100/200)
+# grd = ""
+
+# print("==================")
+# print("==== Results =====")
+# print("Your total Marks are: ", total)
+# print("Your Average is: ", avg)
+# print("Your Percentage is: ", per)
+
+# if per >= 90:
+#     grd = "A+ Grade"
+# elif per >= 80:
+#     grd = "A- Grade"
+# elif per >= 70:
+#     grd = "A Grade"
+# elif per >= 60:
+#     grd = "B Grade"
+# elif per >= 50:
+#     grd = "C Grade"
+# else:
+#     grd = "Failed"
+
+# print("Your Grade is: ", grd)
+# print("==================")
+
+
+
+
+
+'''#COLOUR GAME
+a=input("Enter first colour: ")
+b= input("Enter second colour: ")
+if a=='red' and b=='white':
+    print("When",a,"combines with",b,"it becomes PINK")
+elif a=='green' and b=='red':
+    print("When", a, "combines with", b, "it becomes YELLOW")
+elif a == 'blue' and b == 'white':
+    print("When", a, "combines with", b, "it becomes SKY BLUE")
+elif a=='green' and b=='yellow':
+    print("When", a, "combines with", b, "it becomes PARROT GREEN")
+elif a=='yellow' and b=='red':
+    print("When", a, "combines with", b, "it becomes ORANGE")
+elif a=='green' and b=='blue':
+    print("When", a, "combines with", b, "it becomes BLACK")
+elif a=='pink' and b=='blue':
+    print("When", a, "combines with", b, "it becomes PURPLE")
+else:
+    print("invalid Colour")'''
